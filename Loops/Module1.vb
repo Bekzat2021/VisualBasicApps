2f7d801089e5e1dcec12eea45ec2948eb49a4070
Bekzat2021 <bekzat.work@gmail.com>
2020-08-22 20:44:16 +0600

    Simple random 55 generator

---
+++ Loops/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim rnd As New Random()
+        Dim value As Integer
+
+        For i As Integer = 1 To 200
+            value = rnd.Next(100)
+            If value = 55 Then
+                Console.Write("Value 55 finded in loop ")
+                Console.WriteLine($"at {i} iteration")
+                Exit For
+            ElseIf i = 200 Then
+                Console.WriteLine("Value 55 not found in loop")
+            End If
+        Next
+    End Sub
+
+End Module
