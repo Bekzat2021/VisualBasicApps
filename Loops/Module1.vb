c785b03a0d3d9efdca6b09fac5dd1c5d87204b7f
Bekzat2021 <bekzat.work@gmail.com>
2020-08-23 21:09:22 +0600

    Simple array sorting example

--- Loops/Module1.vb
+++ Loops/Module1.vb
@@ -1,19 +1,32 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim rnd As New Random()
-        Dim value As Integer
 
-        For i As Integer = 1 To 200
-            value = rnd.Next(100)
-            If value = 55 Then
-                Console.Write("Value 55 finded in loop ")
-                Console.WriteLine($"at {i} iteration")
-                Exit For
-            ElseIf i = 200 Then
-                Console.WriteLine("Value 55 not found in loop")
-            End If
+        Dim random As New Random()
+        Dim numbers(10) As Integer
+
+        For i As Integer = 0 To numbers.Length - 1
+            numbers(i) = random.Next(10)
+            Console.Write(numbers(i) & " ")
         Next
+        Console.WriteLine()
+
+        Dim temp As Integer
+        For i As Integer = 0 To numbers.Length - 1
+            For j As Integer = i + 1 To numbers.Length - 1
+                If numbers(i) > numbers(j) Then
+                    temp = numbers(i)
+                    numbers(i) = numbers(j)
+                    numbers(j) = temp
+                End If
+            Next
+        Next
+
+        For i As Integer = 0 To numbers.Length - 1
+            Console.Write(numbers(i) & " ")
+        Next
+        Console.WriteLine()
+
     End Sub
 
 End Module
