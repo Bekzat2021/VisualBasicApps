9c01110bfeee0361fdd4e6e5828c4b83576a30d4
Bekzat2021 <bekzat.work@gmail.com>
2020-08-31 21:33:12 +0600

    My own example with enum

--- Loops/Module1.vb
+++ Loops/Module1.vb
@@ -1,21 +1,40 @@
 ﻿Module Module1
 
-    Sub Main()
+    Enum MathOperations
+        Add = 1
+        Subtract
+        Multiply
+        Devide
+    End Enum
 
-        Console.WriteLine("Введите путь к файлу: ")
-        Dim path As String = Console.ReadLine()
-        Dim number As Integer = Modules.Module2.Read(path)
-        Console.WriteLine("Факториал числа {0} равен {1}", number,
-        Factorial(number))
+    Sub Main()
+        Console.Write("Enter first value: ")
+        Dim value1 As Double = CDbl(Console.ReadLine())
+
+        Console.Write("Enter second value: ")
+        Dim value2 As Double = CDbl(Console.ReadLine())
+
+        Console.Write("Enter operation code: ")
+        Dim operation As MathOperations = CInt(Console.ReadLine())
+
+        Calculate(value1, value2, operation)
 
     End Sub
 
-    Function Factorial(x As Integer) As Integer
-        If x = 1 Then
-            Return 1
-        Else
-            Return x * Factorial(x - 1)
-        End If
-    End Function
+    Sub Calculate(ByVal input1 As Double, ByVal input2 As Double,
+                  opertaion As MathOperations)
+        Dim result As Double = 0
+        Select Case opertaion
+            Case MathOperations.Add
+                result = input1 + input2
+            Case MathOperations.Subtract
+                result = input1 - input2
+            Case MathOperations.Multiply
+                result = input1 * input2
+            Case MathOperations.Devide
+                result = input1 / input2
+        End Select
+        Console.WriteLine("Result of opertion {0} {1} {2} = {3}", input1, opertaion, input2, result)
+    End Sub
 
 End Module
